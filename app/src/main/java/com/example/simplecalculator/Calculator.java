1afcfaaa431470178536c95aef706333b86a1564
Sami Kukkonen <samikukkonen00@hotmail.com>
2022-07-07 19:14:08 +0300

    Tehty käyttöliittymää ja navigaatiota ohjelmaan. Käyttöliittymä kesken vielä paljon.

---
+++ app/src/main/java/com/example/simplecalculator/Calculator.java
@@ -0,0 +1,20 @@
+package com.example.simplecalculator;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class Calculator extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_calculator, container, false);
+    }
+}
---
+++ app/src/main/java/com/example/simplecalculator/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.simplecalculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+
+import com.google.android.material.tabs.TabLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TabLayout tablayout;
+    private ViewPager viewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FragmentNavigation();
+
+    }
+
+
+
+    public void FragmentNavigation(){
+        tablayout = findViewById(R.id.tableLayout);
+        viewPager = findViewById(R.id.viewPager);
+
+        tablayout.setupWithViewPager(viewPager);
+        Adapter adapter = new Adapter(getSupportFragmentManager(), FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+        adapter.addFragment(new Calculator(), "Calculate");
+        adapter.addFragment(new History(), "History");
+        viewPager.setAdapter(adapter);
+    }
+}
